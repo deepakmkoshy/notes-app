5052bd43f5fd2c40d0e41cd0b7fa6bc510151538
Deepak <deepakmkoshy@gmail.com>
2021-02-24 00:36:28 +0530

    Notes app with NodeJS

---
+++ app.js
@@ -0,0 +1,10 @@
+const printing = require('./notes.js')
+const chalk = require('chalk')
+
+const command = process.argv
+console.log(command);
+
+if(command === 'add'){
+    console.log('Adding note')
+}
+
