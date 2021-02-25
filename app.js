06d2fe6715d5b1b1d250ea1f7e695f6963f03180
Deepak <deepakmkoshy@gmail.com>
2021-02-25 23:21:32 +0530

    Using yargs

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const printing = require('./notes.js')
-const chalk = require('chalk')
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv
-console.log(command);
+// console.log(process.argv);
+yargs.version('1.1.0')
 
-if(command === 'add'){
-    console.log('Adding note')
-}
+//Create add comment
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log("added");
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log("Removed");
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Display the list',
+    handler: function(){
+        console.log("List:");
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log("Read");
+    }
+})
+
+
+console.log(yargs.argv)
 
